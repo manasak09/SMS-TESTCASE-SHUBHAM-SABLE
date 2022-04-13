16a9561deab7a8b31eac427a90f05f3648cedff0
Group6 <103427156+manasak09@users.noreply.github.com>
2022-04-13 17:29:08 +0530

    Update IncidentCentreDAO.java

--- src/main/java/com/dao/IncidentCentreDAO.java
+++ src/main/java/com/dao/IncidentCentreDAO.java
@@ -1,8 +1,8 @@
 package com.dao;
 
 import org.springframework.stereotype.Repository;
-import java.util.*;
-import com.model.*;
+import java.util.List;
+import com.model.IncidentCentre;
 
 
 @Repository
